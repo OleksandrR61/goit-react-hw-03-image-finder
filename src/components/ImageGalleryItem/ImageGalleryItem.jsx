b16c32fa7787d98bd70d36f6e1a4d693ccd5fce9
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-02-02 13:21:36 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,7 +1,17 @@
+import PropTypes from 'prop-types';
+
 import styles from './ImageGalleryItem.module.css';
 
 export const ImageGalleryItem = ({image}) => (
     <div className={styles.ImageGalleryItem}>
-        <img className={styles.ImageGalleryItemImage} src={image.webformatURL} alt={image.tags} largeImageURL={image.largeImageURL}/>
+        <img className={styles.ImageGalleryItemImage} src={image.webformatURL} alt={image.tags}/>
     </div>
 )
+
+ImageGalleryItem.propTypes = {
+    image: PropTypes.shape({
+        webformatURL: PropTypes.string.isRequired,
+        tags: PropTypes.string,
+        largeImageURL: PropTypes.string.isRequired,
+    }).isRequired,
+}
