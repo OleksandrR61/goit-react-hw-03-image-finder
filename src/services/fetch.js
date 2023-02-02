95dec50cfda2d1ae38ad90ec3b21bcf43388d333
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-02-02 02:11:07 +0200

    Update fetch.js

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 const pixabayFetch = axios.create({
     baseURL: 'https://some-domain.com/api/',
 });
 
-export const getImages = async () => {
-    const request = await pixabayFetch
-    console.log()
-}
+export const getImages = async ({page, search}) => {
+    const request = await pixabayFetch.get('');
+    console.log(request);
+};
+
+getImages.PropTypes = {
+    params: PropTypes.shape({
+        page: PropTypes.number.isRequired,
+        search: PropTypes.string.isRequired,
+    }),
+};
